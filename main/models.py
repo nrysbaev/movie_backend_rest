a05bc5f6abcca1c91ede351b82acdba6f478c214
nrysbaev <nurkenrysbaev@gmail.com>
2022-01-14 00:57:25 +0600

    lesson 2 | db connections

--- main/models.py
+++ main/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
@@ -9,6 +14,38 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField()
     duration = models.IntegerField()
+    genres = models.ManyToManyField(Genre, blank=True)
+
+    def count_genres(self):
+        return self.genres.filter(is_active=True).count()
+
+    def rating(self):
+        c = 0
+        for i in self.ratings.all():
+            c += i.value
+        try:
+            return c / self.ratings.all().count()
+        except:
+            return 0
 
     def __str__(self):
         return self.name
+
+
+STARS = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+)
+
+
+class Rating(models.Model):
+    text = models.CharField(max_length=100)
+    value = models.IntegerField(choices=STARS)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='ratings')
+
+    def __str__(self):
+        return self.text
--- main/serializers.py
+++ main/serializers.py
@@ -1,16 +1,44 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre, Rating
+
+
+class GenresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class RatingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['id', 'text', 'value']
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    # genres = GenresSerializer(many=True)
+    # ratings = RatingsSerializer(many=True)
+    ratings = serializers.SerializerMethodField()
+    genres = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         # fields = '__all__'
-        fields = 'id name'.split()
-        # fields = ['id', 'name']
+        # fields = 'id name'.split()
+        fields = ['id', 'name', 'genres', 'ratings', 'count_genres', 'rating']
+
+    def get_ratings(self, movie):
+        # rate = movie.ratings.filter(value__gt=3)
+        rate = Rating.objects.filter(movie=movie, value__gt=3)
+        data = RatingsSerializer(rate, many=True).data
+        return data
+
+    def get_genres(self, movie):
+        # filtered_genres = movie.genres.filter(is_active=True)
+        # data = GenresSerializer(filtered_genres, many=True).data
+        return GenresSerializer(movie.genres.filter(is_active=True), many=True).data
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'name', 'description', 'duration']
+        fields = ['id', 'name', 'description', 'duration', 'count_genres']
